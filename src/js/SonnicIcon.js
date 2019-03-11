74eb92e22714b8a9b90898ad9aa01282f5b8adbf
Chesta <chesta@protonmail.com>
2019-03-11 01:48:56 +0100

    Moved icons to separate files

---
+++ src/js/SonnicIcon.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+class SonnicIcon extends Component {
+    render() {
+        return (
+            <SvgIcon>
+                <svg version="1.1" id="Layer_1" viewBox="0 0 256 256">
+                  <path id="XMLID_3_" className="st0" d="M182.3,1.9c0,0-29.6,7-48.1,29c0,0,74.6-19,121.1,46.3c0,0-17.7-13.3-54.2-13.2
+	c0,0,64.6,36.8,53,118.1c0,0-7.6-30.9-29.2-47.8c0,0,20,74.3-46.1,121.6c0,0,15.7-21.8,13.7-54.4c0,0-36.5,65.6-118.8,53.1
+	c0,0,25.8-4.4,48.2-29.1c0,0-70.8,20.5-121.6-46.4c0,0,24,15.9,54.5,13.5c0,0-63.7-33.2-53.1-119.1c0,0,4.2,24.9,29,49.3
+	c0,0-20.3-71.5,46.7-122.5c0,0-16.7,26.6-13.3,55C64.1,55.1,99.9-9.8,182.3,1.9z"/>
+                </svg>
+              </SvgIcon>
+        );
+    }
+}
+
+export default SonnicIcon;
